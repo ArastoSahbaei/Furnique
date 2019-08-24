207561116f4b6f1194a88991b72bc675e645e533
arasto <Arasto.Sahbaei@gmail.com>
2019-08-24 09:39:10 +0200

    folder structure

--- modlyGUI/src/App.js
+++ modlyGUI/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Animation from './animations/Animation.js'
 
 function App() {
   return (
     <div className="App">
       <h1>This is the Modly application :></h1>
+      <Animation/>
     </div>
   );
 }
---
+++ modlyGUI/src/animations/Animation.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Animation.css'
+
+export default function Animation() {
+    return (
+        <div>
+            <h1>What's up fool?</h1>
+        </div>
+    )
+}
