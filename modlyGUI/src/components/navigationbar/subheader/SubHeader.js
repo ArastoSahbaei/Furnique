dc0dc655b9c80af5e150ba3af77880d3d1075ba6
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2019-08-28 16:32:45 +0200

    updated fonts

--- modlyGUI/src/components/navigationbar/subheader/SubHeader.js
+++ modlyGUI/src/components/navigationbar/subheader/SubHeader.js
@@ -5,9 +5,10 @@ export default function SubHeader() {
     return (
         <div>
             <ul className="subHeader">
-                <li className="subHeaderListItem">FRI FRAKT & FRIA RETURER</li>
-                <li className="subHeaderListItem">LEVERANS 3-4 VECKOR</li>
-                <li className="subHeaderListItem">DIREKT IFRÅN VÅR FABRIK</li>
+                <li className="subHeaderListItem"> <i class="fa fa-arrows-h" aria-hidden="true"></i> FRI FRAKT & FRIA RETURER</li>
+                <li className="subHeaderListItem"> <i class="fa fa-clock-o" aria-hidden="true"></i> LEVERANS 3-4 VECKOR</li>
+                <li className="subHeaderListItem"> <i class="fa fa-tree" aria-hidden="true"></i> MILJÖVÄNLIGT</li>
+                <li className="subHeaderListItem"> <i class="fa fa-star-o" aria-hidden="true"></i> NÖJDA KUNDER</li>
             </ul>
         </div>
     )
