c82f8d12181fa1485a5caf4e63d4a842198d5e47
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2019-08-28 14:17:56 +0200

    added social media

--- modlyGUI/src/components/footer/socialmedia/SocialMedia.js
+++ modlyGUI/src/components/footer/socialmedia/SocialMedia.js
@@ -5,10 +5,10 @@ export default function SocialMedia() {
     return (
         <div>
             <ul className="socialMediaUL">
-            <li className="socialMediaLI"><a href="/404">     <i class="fa fa-youtube"      aria-hidden="true"></i></a></li>
-            <li className="socialMediaLI"><a href="/404">     <i class="fa fa-facebook"     aria-hidden="true"></i></a></li>
-            <li className="socialMediaLI"><a href="/404">     <i class="fa fa-instagram"    aria-hidden="true"></i></a></li>
-            <li className="socialMediaLI"><a href="/404">     <i class="fa fa-github"       aria-hidden="true"></i></a></li>
+            <li className="socialMediaLI"><a href="/404">     <i className="fa fa-pinterest"      aria-hidden="true"></i></a></li>
+            <li className="socialMediaLI"><a href="/404">     <i className="fa fa-facebook"     aria-hidden="true"></i></a></li>
+            <li className="socialMediaLI"><a href="/404">     <i className="fa fa-instagram"    aria-hidden="true"></i></a></li>
+            <li className="socialMediaLI"><a href="/404">     <i className="fa fa-twitter"       aria-hidden="true"></i></a></li>
         </ul>
         </div>
     )
