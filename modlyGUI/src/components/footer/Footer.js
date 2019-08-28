ca30bc2a505f53577d2ad2d7bf813c8d25ea1278
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2019-08-28 11:05:26 +0200

    social media

--- modlyGUI/src/components/footer/Footer.js
+++ modlyGUI/src/components/footer/Footer.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Footer.css'
+import SocialMedia from './socialmedia/SocialMedia'
 
 export default function Footer() {
     return (
         <div className="footerWrapper">
                <ul className="footerList">
                     <li className="footerList"> Modly Corporation </li>
+                    <SocialMedia/>
                 </ul>
         </div>
     )
--- modlyGUI/src/components/footer/socialmedia/SocialMedia.js
+++ modlyGUI/src/components/footer/socialmedia/SocialMedia.js
@@ -4,7 +4,12 @@ import './SocialMedia.css'
 export default function SocialMedia() {
     return (
         <div>
-            
+            <ul className="socialMediaUL">
+            <li className="socialMediaLI"><a href="/404">     <i class="fa fa-youtube"      aria-hidden="true"></i></a></li>
+            <li className="socialMediaLI"><a href="/404">     <i class="fa fa-facebook"     aria-hidden="true"></i></a></li>
+            <li className="socialMediaLI"><a href="/404">     <i class="fa fa-instagram"    aria-hidden="true"></i></a></li>
+            <li className="socialMediaLI"><a href="/404">     <i class="fa fa-github"       aria-hidden="true"></i></a></li>
+        </ul>
         </div>
     )
 }
