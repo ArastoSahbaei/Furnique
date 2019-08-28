776d1923d0033605e83526f77ae5c7208247d95e
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2019-08-28 16:09:06 +0200

    formating

--- modlyGUI/src/components/footer/socialmedia/SocialMedia.js
+++ modlyGUI/src/components/footer/socialmedia/SocialMedia.js
@@ -6,9 +6,9 @@ export default function SocialMedia() {
         <div>
             <ul className="socialMediaUL">
             <li className="socialMediaLI"><a href="/404">     <i className="fa fa-pinterest"      aria-hidden="true"></i></a></li>
-            <li className="socialMediaLI"><a href="/404">     <i className="fa fa-facebook"     aria-hidden="true"></i></a></li>
-            <li className="socialMediaLI"><a href="/404">     <i className="fa fa-instagram"    aria-hidden="true"></i></a></li>
-            <li className="socialMediaLI"><a href="/404">     <i className="fa fa-twitter"       aria-hidden="true"></i></a></li>
+            <li className="socialMediaLI"><a href="/404">     <i className="fa fa-facebook"       aria-hidden="true"></i></a></li>
+            <li className="socialMediaLI"><a href="/404">     <i className="fa fa-instagram"      aria-hidden="true"></i></a></li>
+            <li className="socialMediaLI"><a href="/404">     <i className="fa fa-twitter"        aria-hidden="true"></i></a></li>
         </ul>
         </div>
     )
