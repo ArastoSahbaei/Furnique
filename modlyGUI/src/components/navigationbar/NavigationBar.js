85b58ff08529570cb8dd5dbf77dc98b288c636e1
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2019-08-28 16:54:22 +0200

    icons in navigationBar

--- modlyGUI/src/components/navigationbar/NavigationBar.js
+++ modlyGUI/src/components/navigationbar/NavigationBar.js
@@ -10,12 +10,13 @@ export default function NavigationBar() {
             <div className="navigationBarWrapper">
                 <ul className="navigationList">
                     <span className="logotype">MODLY</span>
-                    <li className="itemList"> <NavLink activeClassName='is-active' to="/home">      Home        </NavLink> </li>
-                    <li className="itemList"> <NavLink activeClassName='is-active' to="/about">     About       </NavLink> </li>
+                    <li className="itemList"> <NavLink activeClassName='is-active' to="/home">      Home         </NavLink> </li>
+                    <li className="itemList"> <NavLink activeClassName='is-active' to="/about">     About        </NavLink> </li>
                     <li className="itemList"> <NavLink activeClassName='is-active' to="/expertise"> Animations   </NavLink> </li>
-                    <li className="itemList"> <NavLink activeClassName='is-active' to="/portfolio"> Support   </NavLink> </li>
-                    <li className="itemList"> <NavLink activeClassName='is-active' to="/contact">   Contact     </NavLink> </li>
-                    <span className="shoppingCartIcon">SHOPPINGCART</span>
+                    <li className="itemList"> <NavLink activeClassName='is-active' to="/portfolio"> Support      </NavLink> </li>
+                    <li className="itemList"> <NavLink activeClassName='is-active' to="/contact">   Contact      </NavLink> </li>
+                    <li className="shoppingCartIcon"> <i class="fa fa-shopping-cart" aria-hidden="true"> </i></li>
+                    <li className="myLikesIcon"> <i class="fa fa-heart" aria-hidden="true"> </i></li>
                 </ul>
                 <SubHeader/>
                 <Switch>
