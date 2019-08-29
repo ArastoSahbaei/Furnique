b6a18643db8a024445d002a9981c0cf1ff090935
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2019-08-29 13:34:20 +0200

    secondrow

--- modlyGUI/src/components/navigationbar/subheader/SubHeader.js
+++ modlyGUI/src/components/navigationbar/subheader/SubHeader.js
@@ -7,7 +7,7 @@ export default function SubHeader() {
             <ul className="subHeader">
                 <li className="subHeaderListItem"> <i class="fa fa-arrows-h" aria-hidden="true"></i> FRI FRAKT & FRIA RETURER</li>
                 <li className="subHeaderListItem"> <i class="fa fa-clock-o" aria-hidden="true"></i> LEVERANS 3-4 VECKOR</li>
-                <li className="subHeaderListItem"> <i class="fa fa-tree" aria-hidden="true"></i> MILJÖVÄNLIGT</li>
+                <li className="subHeaderListItem"> <i class="fa fa-tree" aria-hidden="true"></i> MILJÖSMART</li>
                 <li className="subHeaderListItem"> <i class="fa fa-star-o" aria-hidden="true"></i> NÖJDA KUNDER</li>
             </ul>
         </div>
--- modlyGUI/src/pages/home/Home.js
+++ modlyGUI/src/pages/home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Home.css'
 import TreeLogo from '../../shared/images/denne.jpg';
+import Arrow from '../../shared/images/arrow.png';
 
 
 export default function Home() {
@@ -9,6 +10,17 @@ export default function Home() {
             <img className="treeLogo" src={TreeLogo}/>
             <p className="title">Vi klimatkompenserar till 100% <br/> för varje möbel vi tillverkar.</p>
             <p className="subTitle">LÄS MER OM HUR</p>
+
+                            <div className="produktWrapper">
+                            <img className="arrowLogo" src={Arrow}/>
+                                <p className="produktTitle">Designa din möbel</p>
+                                <ul className="produktUL">
+                                   {/*  <li className="produktTitle">Våra Produkter</li> */}
+                                    <li className="produktLI">MATBORD</li>
+                                    <li className="produktLI">SOFFBORD</li>
+                                    <li className="produktLI">SIDOBORD</li>
+                                </ul>
+                            </div>
         </div>
     )
 }
