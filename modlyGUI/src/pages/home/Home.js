0062235e63218f77620ddbb1cfe7b0b5dbe1f2bf
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2019-08-29 12:30:36 +0200

    formating

--- modlyGUI/src/pages/home/Home.js
+++ modlyGUI/src/pages/home/Home.js
@@ -6,9 +6,8 @@ import TreeLogo from '../../shared/images/denne.jpg';
 export default function Home() {
     return (
         <div className="homeWrapper">
-            <p className="title">Vi klimatkompenserar till 100% <br/>
-            för varje möbel vi tillverkar.</p>
             <img className="treeLogo" src={TreeLogo}/>
+            <p className="title">Vi klimatkompenserar till 100% <br/> för varje möbel vi tillverkar.</p>
             <p className="subTitle">LÄS MER OM HUR</p>
         </div>
     )
