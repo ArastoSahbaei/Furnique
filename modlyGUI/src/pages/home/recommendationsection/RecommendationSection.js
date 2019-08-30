c1e9fdc8ecb107b233d7bd4fd7bbe983d295a185
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2019-08-30 11:27:31 +0200

    new styles and material added

--- modlyGUI/src/pages/home/recommendationsection/RecommendationSection.js
+++ modlyGUI/src/pages/home/recommendationsection/RecommendationSection.js
@@ -5,35 +5,39 @@ import CardHeader from '@material-ui/core/CardHeader'
 import CardContent from '@material-ui/core/CardContent'
 import Typography from '@material-ui/core/Typography'
 import Avatar from '@material-ui/core/Avatar'
-import user1 from "../../../shared/images/arrow.png"
+import user1 from "../../../shared/images/user1.jfif"
+import user2 from "../../../shared/images/user2.jfif"
+import user3 from "../../../shared/images/user3.jfif"
 
 
 export default function RecommendationSection() {
     return (
         <div className="recommendationSectionWrapper">
 
-            <p className="recommendationTitle">Word!</p>
+            <p className="recommendationTitle">Recensioner</p>
 
 
             <Card className="card1">
                 <CardHeader avatar={ <Avatar aria-label="Recipe">  <img className="image1"  src={user1} alt="Eng" /> </Avatar> }
-                title="Jenny Hill"/>
+                title="Elisabeth Vikander"/>
                 <CardContent>
-                <Typography component="p"> "Finally a dining table that fits my long, but narrow kitchen. i love the scandinavian design!" </Typography>
+                <Typography component="p"> "Vivamus laoreet ante vel tellus aliquet, ac fringilla leo condimentum. Mauris in massa malesuada, fringilla massa accumsan, imperdiet sapien. Vestibulum suscipit blandit commodo. Nulla facilisi. Pellentesque in nisl diam." </Typography>
                 </CardContent>
             </Card>
+
             <Card className="card2">
-                <CardHeader avatar={ <Avatar aria-label="Recipe">  <img className="image1"  src={user1} alt="Eng" /> </Avatar> }
+                <CardHeader avatar={ <Avatar aria-label="Recipe">  <img className="image1"  src={user2} alt="Eng" /> </Avatar> }
                 title="Arasto Sahbaei"/>
                 <CardContent>
-                <Typography component="p"> "Finally a dining table that fits my long, but narrow kitchen. i love the scandinavian design!" </Typography>
+                <Typography component="p"> "Vivamus laoreet ante vel tellus aliquet, ac fringilla leo condimentum. Mauris in massa malesuada, fringilla massa accumsan, imperdiet sapien. Vestibulum suscipit blandit commodo. Nulla facilisi. Pellentesque in nisl diam." </Typography>
                 </CardContent>
             </Card>
+
             <Card className="card3">
-                <CardHeader avatar={ <Avatar aria-label="Recipe">  <img className="image1"  src={user1} alt="Eng" /> </Avatar> }
-                title="Mario Jose"/>
+                <CardHeader avatar={ <Avatar aria-label="Recipe">  <img className="image1"  src={user3} alt="Eng" /> </Avatar> }
+                title="Amanda Cronberg"/>
                 <CardContent>
-                <Typography component="p"> "Finally a dining table that fits my long, but narrow kitchen. i love the scandinavian design!" </Typography>
+                <Typography component="p"> "Vivamus laoreet ante vel tellus aliquet, ac fringilla leo condimentum. Mauris in massa malesuada, fringilla massa accumsan, imperdiet sapien. Vestibulum suscipit blandit commodo. Nulla facilisi. Pellentesque in nisl diam." </Typography>
                 </CardContent>
             </Card>
         </div>
