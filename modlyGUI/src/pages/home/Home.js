4cb13dbfc04877106c1ef4d2acf717a6b03334f1
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2019-08-30 13:39:07 +0200

    typo fix

--- modlyGUI/src/pages/home/Home.js
+++ modlyGUI/src/pages/home/Home.js
@@ -9,7 +9,7 @@ export default function Home() {
     return (
         <div className="homeWrapper">
             <img className="treeLogo" src={TreeLogo}/>
-            <p className="title">Vi klimatkompenserar till 100% <br/> för varje möbel vi tillverkar.</p>
+            <p className="title">Vi klimatkompenserar till 100% <br/> för varje bord vi tillverkar.</p>
             <p className="subTitle">LÄS MER OM HUR</p>
             <DesignSection/>
             <RecommendationSection/>
