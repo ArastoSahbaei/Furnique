d24ea7e34b5671f297b39f7f04c42c45dc5221af
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2019-09-04 15:45:47 +0200

    replaced

--- modlyGUI/src/pages/shop/Shop.js
+++ modlyGUI/src/pages/shop/Shop.js
@@ -6,7 +6,7 @@ import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 export default function Shop() {
 
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(105, (window.innerWidth / window.innerHeight), 0.1, 1000);
+    const camera = new THREE.PerspectiveCamera(35, (window.innerWidth / window.innerHeight), 0.1, 1000);
     camera.position.z = 8;
     camera.position.y = 4;
     camera.lookAt( scene.position );
@@ -21,43 +21,47 @@ export default function Shop() {
     renderer.setClearColor(0xb7d6fb); 
     renderer.setSize(window.innerWidth, window.innerHeight)
     document.body.append(renderer.domElement);
-    const boxWidth = 2; const boxHeight = 1; const boxDepth = 1;
+
+    const boxWidth = 1; const boxHeight = 0.1; const boxDepth = 1;
     const tableBoardGeometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
     const textureLoader = new THREE.TextureLoader();
     const customPicture = textureLoader.load('https://threejsfundamentals.org/threejs/lessons/resources/images/compressed-but-large-wood-texture.jpg')
     const tableBoardMaterial = new THREE.MeshLambertMaterial({map: customPicture})
     const tableBoard = new THREE.Mesh(tableBoardGeometry, tableBoardMaterial)
-    tableBoard.position.set(0, 2, 0)
-    tableBoard.scale.set(4, 0.2, 4)
+    tableBoard.position.set(0, 0, 0)
+/*     tableBoard.scale.set(4, 0.2, 4) */
     /* tableBoard.rotation.set(0, 4, 0)  */
 
-    
-    const tableLegsGeometry = new THREE.BoxGeometry(0.3, 10, 0.3);
+    const tableLegPosition = (boxWidth * 0.45); //Positioning the legs
+    const tableLegHeight = 0.8 // Height of the legs
+    const tableLegHeightPosition = (tableLegHeight/2) //Keeping the leg position under table (avoiding the leg to cut through the table)
+
+    const tableLegsGeometry = new THREE.BoxGeometry(0.05, tableLegHeight, 0.05);
     const tableLegsMaterial = new THREE.MeshLambertMaterial(({map: customPicture}))
     const tableLeg1 = new THREE.Mesh(tableLegsGeometry, tableLegsMaterial)
     const tableLeg2 = new THREE.Mesh(tableLegsGeometry, tableLegsMaterial)
     const tableLeg3 = new THREE.Mesh(tableLegsGeometry, tableLegsMaterial)
     const tableLeg4 = new THREE.Mesh(tableLegsGeometry, tableLegsMaterial)
-    tableLeg1.position.set(0.9, -6, -0.4)
-    tableLeg2.position.set(-0.9, -6, -0.4)
-    tableLeg3.position.set(-0.9, -6, 0.4)
-    tableLeg4.position.set(0.9, -6, 0.4)
-    tableLeg1.scale.set(0.1, 1.1, 0.1)
+    tableLeg1.position.set(tableLegPosition, -tableLegHeightPosition, -0.4)
+    tableLeg2.position.set(-tableLegPosition, -tableLegHeightPosition, -0.4)
+    tableLeg3.position.set(-tableLegPosition, -tableLegHeightPosition, 0.4)
+    tableLeg4.position.set(tableLegPosition, -tableLegHeightPosition, 0.4)
+/*  tableLeg1.scale.set(0.1, 1.1, 0.1)
     tableLeg2.scale.set(0.1, 1.1, 0.1)
     tableLeg3.scale.set(0.1, 1.1, 0.1)
-    tableLeg4.scale.set(0.1, 1.1, 0.1)
+    tableLeg4.scale.set(0.1, 1.1, 0.1) */
 
     const floorGeometry = new THREE.PlaneGeometry( 1000, 1000, 10, 10 );
     floorGeometry.rotateX( - Math.PI / 2 );
-/*     const floorTexture = new THREE.TextureLoader().load('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOOcWSD0K2mPwokAFfZIhq5Xl49bh8B17RlU6NqCGa4UOKydgX');
+/*  const floorTexture = new THREE.TextureLoader().load('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOOcWSD0K2mPwokAFfZIhq5Xl49bh8B17RlU6NqCGa4UOKydgX');
     floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping;
     floorTexture.repeat.set(20, 20); */
     const floorMaterial = new THREE.MeshBasicMaterial({color: 0x7c7d81}),
     floor = new THREE.Mesh( floorGeometry, floorMaterial );
-   
+
     const group = new THREE.Group();
     tableBoard.add(tableLeg1, tableLeg2, tableLeg3, tableLeg4);
-    scene.add(tableBoard, floor);
+    scene.add(tableBoard);
 
     const lightAndShadow = () => {
         const ambientLight = new THREE.AmbientLight(0xffffff, 0.75);
@@ -93,7 +97,12 @@ export default function Shop() {
    
     return (
         <div>
-            {console.log(window.innerWidth)}
+            {console.log("tableBoard Position:")}
+            {console.log(tableBoard.position)}
+            {console.log("tableLeg Position:")}
+            {console.log(tableLeg1.position)}
+            {console.log("floor Position:")}
+            {console.log(floor.position)}
         </div>
     )
 
