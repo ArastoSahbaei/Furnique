3997b30d8ba57d62c0204c8855039a59f870d38f
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2019-09-05 10:43:11 +0200

    added clarity variables

--- modlyGUI/src/pages/shop/Shop.js
+++ modlyGUI/src/pages/shop/Shop.js
@@ -46,7 +46,7 @@ export default function Shop() {
     tableLeg3.position.set(-tableLegPosition, -tableLegHeightPosition, 0.4)
     tableLeg4.position.set(tableLegPosition, -tableLegHeightPosition, 0.4)
 
-    const floorGeometry = new THREE.PlaneGeometry( 1000, 1000, 10, 10 );
+    const floorGeometry = new THREE.PlaneGeometry( 80, 80, 10, 10 );
     floorGeometry.rotateX( - Math.PI / 2 );
     const floorTexture = new THREE.TextureLoader().load('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOOcWSD0K2mPwokAFfZIhq5Xl49bh8B17RlU6NqCGa4UOKydgX');
     floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping;
@@ -58,7 +58,8 @@ export default function Shop() {
 
     const lightAndShadow = () => {
         const ambientLight = new THREE.AmbientLight(0xffffff, 0.75);
-        const light = new THREE.PointLight(0xffffff, 1.3, 18);
+        const lightIntensity = 1.3; const lightDistance = 18;
+        const light = new THREE.PointLight(0xffffff, lightIntensity, lightDistance);
         light.position.set(1, 1, 1)
         light.castShadow = true
         light.shadow.camera.near = 0.7
@@ -69,7 +70,7 @@ export default function Shop() {
 
     const displayObjects = () => {
         tableBoard.add(tableLeg1, tableLeg2, tableLeg3, tableLeg4);
-        scene.add(tableBoard);
+        scene.add(tableBoard, floor);
     }
 
     const orbitCamera = () => {
