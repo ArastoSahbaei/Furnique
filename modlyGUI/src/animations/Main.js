041df6de315c4e2818d26474257c6aec5062e5c0
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2019-09-10 16:33:15 +0200

    m

--- modlyGUI/src/animations/Main.js
+++ modlyGUI/src/animations/Main.js
@@ -44,6 +44,10 @@ export default class Main extends Component {
   startAnimationLoop = () => {
     const tableBoard = this.scene.getObjectByName('tableSurface');
     tableBoard.rotation.y += this.title.RotationSpeed;
+    tableBoard.scale.x = this.title.TableWidth;
+    tableBoard.scale.z = this.title.TableHeight;
+    tableBoard.scale.y = this.title.TableDepth;
+
     this.renderer.render(this.scene, this.camera);
     this.requestID = window.requestAnimationFrame(this.startAnimationLoop);
   };
@@ -59,10 +63,16 @@ export default class Main extends Component {
    userGUI = () => {
    this.gui = new dat.GUI();
    var controls = function() {
+    this.TableWidth = 1;
+    this.TableHeight = 1;
+    this.TableDepth = 1;
    this.RotationSpeed = 0.005;
 }
-
   this.title = new controls();
+  this.surface = this.gui.addFolder('Surface')
+  this.surface.add(this.title, 'TableWidth', 1, 3);
+  this.surface.add(this.title, 'TableHeight', 1, 3);
+  this.surface.add(this.title, 'TableDepth', 1, 3);
   this.gui.add(this.title, 'RotationSpeed', 0.005, 0.1);
   }
 
