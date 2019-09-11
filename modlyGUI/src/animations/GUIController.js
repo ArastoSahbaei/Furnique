15e5696c990ba0120257fd6b158f995cb23a062f
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2019-09-11 09:57:44 +0200

    m

--- modlyGUI/src/animations/GUIController.js
+++ modlyGUI/src/animations/GUIController.js
@@ -1,10 +1,12 @@
 import * as dat from 'dat.gui';
 
-export default ({ guis }) => {
+export default ({ gui, title }) => {
      var gui = new dat.GUI();
      var controls = function() {
      this.RotationSpeed = 0.005;
     }
+
      var title = new controls();
      gui.add(title, 'RotationSpeed', 0.005, 0.1);
-    }
+     console.log(title.RotationSpeed)
+}
