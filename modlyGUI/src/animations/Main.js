154e5a99c8901f1680795825a5b655961e2d53fe
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2019-09-17 16:29:01 +0200

    working

--- modlyGUI/src/animations/Main.js
+++ modlyGUI/src/animations/Main.js
@@ -15,7 +15,7 @@ const style = {
 
 export default class Main extends Component {
   
-componentDidMount() {
+  componentDidMount() {
     this.sceneSetup();
     addLights({ scene: this.scene });
     addWallMesh({ scene: this.scene })
@@ -39,10 +39,9 @@ componentDidMount() {
 
     this.scene = new THREE.Scene();
     this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
-    this.camera.position.x = 0; this.camera.position.y = 5; this.camera.position.z = 10;
-    this.camera.lookAt(0, 0, 0);
-/*     this.helper = new THREE.CameraHelper( this.camera );
-    this.scene.add( this.helper ); */
+    this.camera.position.x = 0; this.camera.position.y = 2; this.camera.position.z = 3;
+    this.helper = new THREE.CameraHelper( this.camera );
+    this.scene.add( this.helper ); 
     this.controls = new OrbitControls(this.camera, this.el);
     this.renderer = new THREE.WebGLRenderer({antialias: true});
     this.renderer.setSize(width, height);
@@ -61,25 +60,25 @@ componentDidMount() {
   
 
   startAnimationLoop = () => {
-    const tableBoard = this.scene.getObjectByName('tableSurface');
-    tableBoard.rotation.y += this.title.RotationSpeed;
-    tableBoard.scale.x = this.title.TableWidth;
-    tableBoard.scale.z = this.title.TableHeight;
-    tableBoard.scale.y = this.title.TableDepth;
+    const tableSurface = this.scene.getObjectByName('tableSurface');
+    tableSurface.rotation.y += this.title.Rotation;
+    tableSurface.scale.x = this.title.Bordsbredd;
+    tableSurface.scale.z = this.title.Bordslängd;
+    tableSurface.scale.y = this.title.Bordsdjup;
 
     const tableLeg1 = this.scene.getObjectByName('tableLeg1');const tableLeg2 = this.scene.getObjectByName('tableLeg2');const tableLeg3 = this.scene.getObjectByName('tableLeg3');const tableLeg4 = this.scene.getObjectByName('tableLeg4');
-    tableLeg1.scale.x = this.title.LegsWidth;  tableLeg2.scale.x = this.title.LegsWidth;  tableLeg3.scale.x = this.title.LegsWidth;  tableLeg4.scale.x = this.title.LegsWidth;
-    tableLeg1.scale.z = this.title.LegsHeight; tableLeg2.scale.z = this.title.LegsHeight; tableLeg3.scale.z = this.title.LegsHeight; tableLeg4.scale.z = this.title.LegsHeight;
-    tableLeg1.scale.y = this.title.LegsDepth;  tableLeg2.scale.y = this.title.LegsDepth;  tableLeg3.scale.y = this.title.LegsDepth;  tableLeg4.scale.y = this.title.LegsDepth;
+    tableLeg1.scale.x = this.title.BenBredd;  tableLeg2.scale.x = this.title.BenBredd;  tableLeg3.scale.x = this.title.BenBredd;  tableLeg4.scale.x = this.title.BenBredd;
+    tableLeg1.scale.z = this.title.BenLängd; tableLeg2.scale.z = this.title.BenLängd; tableLeg3.scale.z = this.title.BenLängd; tableLeg4.scale.z = this.title.BenLängd;
+    tableLeg1.scale.y = this.title.BenHöjd;  tableLeg2.scale.y = this.title.BenHöjd;  tableLeg3.scale.y = this.title.BenHöjd;  tableLeg4.scale.y = this.title.BenHöjd;
 
-    tableLeg1.position.x = -this.title.LegPositionX; tableLeg2.position.x = this.title.LegPositionX; tableLeg3.position.x = -this.title.LegPositionX; tableLeg4.position.x = this.title.LegPositionX;
-    tableLeg1.position.z = this.title.LegPositionZ; tableLeg2.position.z = -this.title.LegPositionZ; tableLeg3.position.z = -this.title.LegPositionZ; tableLeg4.position.z = this.title.LegPositionZ;
+    tableLeg1.position.x = -this.title.BenPositionX; tableLeg2.position.x = this.title.BenPositionX; tableLeg3.position.x = -this.title.BenPositionX; tableLeg4.position.x = this.title.BenPositionX;
+    tableLeg1.position.z = this.title.BenPositionY; tableLeg2.position.z = -this.title.BenPositionY; tableLeg3.position.z = -this.title.BenPositionY; tableLeg4.position.z = this.title.BenPositionY;
     
-    tableBoard.position.y = this.title.LegsDepth * 0.65;
-    tableLeg1.position.y = -this.title.LegsDepth * 0.65; 
-    tableLeg2.position.y = -this.title.LegsDepth * 0.65; 
-    tableLeg3.position.y = -this.title.LegsDepth * 0.65; 
-    tableLeg4.position.y = -this.title.LegsDepth * 0.65; 
+    tableSurface.position.y = this.title.BenHöjd * 0.65;
+    tableLeg1.position.y = -this.title.BenHöjd * 0.65; 
+    tableLeg2.position.y = -this.title.BenHöjd * 0.65; 
+    tableLeg3.position.y = -this.title.BenHöjd * 0.65; 
+    tableLeg4.position.y = -this.title.BenHöjd * 0.65; 
 
     this.renderer.render(this.scene, this.camera);
     this.requestID = window.requestAnimationFrame(this.startAnimationLoop);
@@ -87,50 +86,40 @@ componentDidMount() {
 
   userGUI = () => {
     const getTableSurface = this.scene.getObjectByName('tableSurface');
-
-    const params = [
-      texture1(),
-      texture2()
-    ]
+    const params = [texture1(), texture2()]
     
     const update = {
-      updateMaterial: function() {
-        getTableSurface.material.dispose()
-        getTableSurface.material.map = params[0];
-        getTableSurface.material.map.needsUpdate = true;
-      }
+      updateMaterial: function() { getTableSurface.material.map = params[0]; }
     }
 
     this.gui = new dat.GUI();
     const controls = function() {
-
-
-    this.TableWidth = 1.5;
-    this.TableHeight = 1.5;
-    this.TableDepth = 0.6;
-    this.LegsWidth = 2;
-    this.LegsHeight = 2;
-    this.LegsDepth = 1.3;
-    this.LegPositionX = 0.65;
-    this.LegPositionZ = 0.65;
-    this.RotationSpeed = 0.00;
+    this.Bordsbredd = 1.5;
+    this.Bordslängd = 1.5;
+    this.Bordsdjup = 0.6;
+    this.BenBredd = 2;
+    this.BenLängd = 2;
+    this.BenHöjd = 1.3;
+    this.BenPositionX = 0.65;
+    this.BenPositionY = 0.65;
+    this.Rotation = 0.001;
 }
 
     this.title = new controls();
-    this.surface = this.gui.addFolder('Surface')
+    this.surface = this.gui.addFolder('Yta')
 
-    this.surface.add(this.title, 'TableWidth', 0.5, 3);
-    this.surface.add(this.title, 'TableHeight', 0.5, 3);
-    this.surface.add(this.title, 'TableDepth', 0.5, 2);
+    this.surface.add(this.title, 'Bordsbredd', 0.5, 3);
+    this.surface.add(this.title, 'Bordslängd', 0.5, 3);
+    this.surface.add(this.title, 'Bordsdjup', 0.5, 2);
 
-    this.legs = this.gui.addFolder('Legs')
-    this.legs.add(this.title, 'LegsWidth', 1, 3);
-    this.legs.add(this.title, 'LegsHeight', 1, 3);
-    this.legs.add(this.title, 'LegsDepth', 0.1, 2);
-    this.legs.add(this.title, 'LegPositionX', 0.5, 0.67);
-    this.legs.add(this.title, 'LegPositionZ', 0.5, 0.67);
+    this.legs = this.gui.addFolder('Ben')
+    this.legs.add(this.title, 'BenBredd', 1, 3);
+    this.legs.add(this.title, 'BenLängd', 1, 3);
+    this.legs.add(this.title, 'BenHöjd', 0.1, 2);
+    this.legs.add(this.title, 'BenPositionX', 0.5, 0.67);
+    this.legs.add(this.title, 'BenPositionY', 0.5, 0.67);
 
-    this.gui.add(this.title, 'RotationSpeed', 0.0, 0.025);
+    this.gui.add(this.title, 'Rotation', 0.0, 0.01);
     this.gui.add(update, 'updateMaterial')
   }
 
