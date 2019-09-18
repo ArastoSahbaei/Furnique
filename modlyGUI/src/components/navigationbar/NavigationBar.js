469b2dd20767465deb98ad43333d04747723b190
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2019-09-18 14:39:14 +0200

    update

--- modlyGUI/src/components/navigationbar/NavigationBar.js
+++ modlyGUI/src/components/navigationbar/NavigationBar.js
@@ -4,7 +4,6 @@ import './NavigationBar.css'
 import Spinner from '../../shared/images/spinner.gif'
 import SubHeader from './subheader/SubHeader.js'
 const Home = lazy(() => import('../../pages/home/Home'));
-const Design = lazy(() => import('../../pages/shop/Shop'));
 const Container = lazy(() => import('../../animations/Container'));
 
 export default function NavigationBar() {
@@ -13,23 +12,21 @@ export default function NavigationBar() {
             <div className="navigationBarWrapper">
                 <ul className="navigationList">
                     <span className="logotype">Modlify</span>
-                    <li className="itemList"> <NavLink activeClassName='is-active' to="/home">      Hem         </NavLink> </li>
-                    <li className="itemList"> <NavLink activeClassName='is-active' to="/about">     Design        </NavLink> </li>
-                    <li className="itemList"> <NavLink activeClassName='is-active' to="/expertise"> Animation   </NavLink> </li>
-                    <li className="itemList"> <NavLink activeClassName='is-active' to="/portfolio"> Inspiration      </NavLink> </li>
-                    <li className="itemList"> <NavLink activeClassName='is-active' to="/contact">   Kontakt      </NavLink> </li>
+                    <li className="itemList"> <NavLink activeClassName='is-active' to="/home">          Hem          </NavLink> </li>
+                    <li className="itemList"> <NavLink activeClassName='is-active' to="/tables">        Bord         </NavLink> </li>
+                    <li className="itemList"> <NavLink activeClassName='is-active' to="/inspiration">   Inspiration  </NavLink> </li>
+                    <li className="itemList"> <NavLink activeClassName='is-active' to="/kontakt">       Kontakt      </NavLink> </li>
                     <li className="shoppingCartIcon"> <i className="fa fa-shopping-cart" aria-hidden="true"> </i></li>
-                    <li className="myLikesIcon"> <i className="fa fa-user" aria-hidden="true"> </i></li>
+                    <li className="myLikesIcon">      <i className="fa fa-user"          aria-hidden="true"> </i></li>
                 </ul>
                 <SubHeader/>
 
                 <Suspense fallback={<img src={Spinner} alt="SpinnerLogo"/>}>
                     <Switch>
                         <Route exact path="/home"       component={Home} />
-                        <Route exact path="/about"      component={Design} />
-                        <Route exact path="/expertise"  component={Container} />
-                        <Route exact path="/portfolio"  component={Home} />
-                        <Route exact path="/contact"    component={Home} />
+                        <Route exact path="/tables"     component={Container} />
+                        <Route exact path="/inspiration"  component={Home} />
+                        <Route exact path="/kontakt"    component={Home} />
                         <Route component={Home} />
                     </Switch>
                 </Suspense>
