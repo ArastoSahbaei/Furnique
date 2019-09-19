52c0c9b536d71c19104a402c83d8ff55205e70cc
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2019-09-19 13:00:54 +0200

    m

--- modlyGUI/src/components/navigationbar/subheader/SubHeader.js
+++ modlyGUI/src/components/navigationbar/subheader/SubHeader.js
@@ -6,23 +6,23 @@ export default function SubHeader() {
         <div>
             <ul className="subHeader">
                 <li className="fraktFritt"> <i class="fas fa-truck"></i>         
-                <span className="headerSubTitle">Frakfritt</span> 
+                <span className="headerSubTitle">Fri frakt</span> 
                 <p className="fraktFrittSubTitle">2-3 Veckors leveranstid</p>
                 </li>
 
                 <li className="trygghetsGaranti"> <i class="far fa-smile-beam"></i>    
                 <span className="headerSubTitle">Trygghetsgaranti</span> 
-                <p className="fraktFrittSubTitle">100 dagars returrätt</p>
+                <p className="fraktFrittSubTitle">Alltid 100 dagars returrätt</p>
                 </li>
 
                 <li className="miljoSmart"> <i class="fab fa-pagelines"></i>     
                 <span className="headerSubTitle">Miljösmart</span> 
-                <p className="fraktFrittSubTitle">Vartenda köp bidrar till miljön, läs mer om hur här.</p>
+                <p className="fraktFrittSubTitle">Vartenda köp bidrar till miljön. Läs mer om hur här</p>
                 </li>
 
                 <li className="betalningsMetoder"> <i class="fab fa-bitcoin"></i>       
                 <span className="headerSubTitle">Säkra betalningsmetoder</span> 
-                <p className="fraktFrittSubTitle">Säkert, tryggt & smidigt oavsett kort eller faktura.</p>
+                <p className="fraktFrittSubTitle">Säkert, tryggt & smidigt oavsett kort eller faktura</p>
                 </li>
             </ul>
         </div>
