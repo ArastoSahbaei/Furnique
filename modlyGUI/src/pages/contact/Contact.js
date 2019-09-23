83241ac4470dad35fa66de44d5dc17f865e1a3ef
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2019-09-23 09:10:56 +0200

    m

--- modlyGUI/src/pages/contact/Contact.js
+++ modlyGUI/src/pages/contact/Contact.js
@@ -5,6 +5,8 @@ export default function Contact() {
     return (
         <div>
                 <h1>What'sup fooolio?</h1>
+                add checkout page <br></br>
+                create a button that adds to checkout
         </div>
     )
 }
