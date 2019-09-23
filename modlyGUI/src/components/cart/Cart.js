208ff8eab91a0074815fd23d471f7b3b12b98eb8
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2019-09-23 12:59:50 +0200

    cart updated with className

--- modlyGUI/src/components/cart/Cart.js
+++ modlyGUI/src/components/cart/Cart.js
@@ -5,11 +5,15 @@ import './Cart.css'
 export default function Cart() {
     return (
         <div>
-            <Menu right width={350} isOpen={false} id={"testi"} className={ "my-menu" } customBurgerIcon={ <i className="fa fa-shopping-cart" aria-hidden="true"/> }>
+            <Menu right width={350} 
+                  isOpen={false} 
+                  id={"testi"} className={ "my-menu" } 
+                  customBurgerIcon={<i className="fa fa-shopping-cart" aria-hidden="true"/>}
+                  burgerButtonClassName={ "cartButton" }>
+
                     <a id="home" className="menu-item" href="/">Home</a>
                     <a id="about" className="menu-item" href="/about">About</a>
                     <a id="contact" className="menu-item" href="/contact">Contact</a>
-                    <a className="menu-item--small" href="">Settings</a>
                   </Menu>   
         </div>
     )
