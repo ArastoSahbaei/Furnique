56b4bc28170f2a1f016183b6a224dbf888e3a61d
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2019-09-23 15:05:52 +0200

    removed crap

--- modlyGUI/src/pages/contact/Contact.js
+++ modlyGUI/src/pages/contact/Contact.js
@@ -3,7 +3,6 @@ import './Contact.css'
 import { Cart } from './Cart.js'
 import { CartProvider } from './CartContext'
 import { TshirtList } from './TshirtList'
-import { slide as Menu } from 'react-burger-menu'
 
 export default function Contact() {
 
