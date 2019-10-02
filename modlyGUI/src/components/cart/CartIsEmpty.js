eac66549bdf0da29bfe5826c0d886d7fb31a019c
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2019-10-02 15:50:46 +0200

    m

--- modlyGUI/src/components/cart/CartIsEmpty.js
+++ modlyGUI/src/components/cart/CartIsEmpty.js
@@ -3,7 +3,9 @@ import React from 'react'
 const CartIsEmpty = () => {
     return (
         <div>
-            <h1>Your Cart Is UNfortunatley empty bro</h1>
+            <h1>Your Cart Is empty bro :(</h1>
+            <span>In need of new furniture? 
+                <button>Browse here.</button></span>
         </div>
     )
 }
