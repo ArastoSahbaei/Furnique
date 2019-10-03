e6780f97b5b79178e9f18b63806ec1802047d6f2
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2019-10-03 16:00:10 +0200

    m

--- modlyGUI/src/components/cart/cartisempty/CartIsEmpty.js
+++ modlyGUI/src/components/cart/cartisempty/CartIsEmpty.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import './CartIsEmpty.css'
+import Spinneeer from '../../../shared/images/empty_cart.png'
+import RainDrop from '../../../shared/images/raindrop.png'
 
 const CartIsEmpty = () => {
     return (
         <div>
-            <h1>Your Cart Is empty bro :(</h1>
-            <span>In need of new furniture? 
-                <button>Browse here.</button></span>
+              <img className="rainDropImage" src={RainDrop} alt="SpinnereeLogo"/>
+              <img className="cartIsEmptyLogo" src={Spinneeer} alt="SpinnereeLogo"/>
+                    <h1>The cart is empty</h1>
+                    <h3>In need of new furniture?</h3>
+                    <button className="browseProducts"> <span>Browse products </span> </button> <br/>
+                    <button className="createYourOwn">  <span>Design your own </span> </button> <br/>
+                    <button className="getInspired">    <span>Get inspiration </span> </button> <br/>
         </div>
     )
 }
