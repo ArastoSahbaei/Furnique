f98530342ea672e1139743090708cdf65bb27921
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2019-10-07 09:20:56 +0200

    m

--- modlyGUI/src/pages/home/Home.js
+++ modlyGUI/src/pages/home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './Home.css'
-import TreeLogo from '../../shared/images/denne.jpg';
+import TreeLogo from '../../shared/images/haha.gif';
+import MainPic from '../../shared/images/room.jpg';
 import Swiper from '../../components/swiper/Swiper'
 
 
 export default function Home() {
     return (
         <div className="homeWrapper">
+            <img className="mainPic" src={MainPic} alt=""/>
             <img className="treeLogo" src={TreeLogo} alt=""/>
             <p className="title">Vi klimatkompenserar till 100% <br/> för varje bord vi tillverkar.</p>
             <p className="subTitle">LÄS MER OM HUR</p>
