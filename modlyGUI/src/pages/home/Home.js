2f35383b7a2c5f73a83183bee034a33830f0ffd1
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2019-10-07 11:40:55 +0200

    m

--- modlyGUI/src/pages/home/Home.js
+++ modlyGUI/src/pages/home/Home.js
@@ -9,6 +9,9 @@ export default function Home() {
     return (
         <div className="homeWrapper">
             <img className="mainPic" src={MainPic} alt=""/>
+            <span className="mainPicTitle">Självvalda mått, <br/> med hjälp ifrån utvalda hantverkare.</span>
+            <button className="createTableButton"><i class="fas fa-hammer"></i> SKAPA DITT BORD</button>
+
             <img className="treeLogo" src={TreeLogo} alt=""/>
             <p className="title">Vi klimatkompenserar till 100% <br/> för varje bord vi tillverkar.</p>
             <p className="subTitle">LÄS MER OM HUR</p>
