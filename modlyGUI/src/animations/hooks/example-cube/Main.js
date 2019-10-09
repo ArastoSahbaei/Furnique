cb8c5b089e42e05e31aa1d69badd670dbde91590
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2019-10-09 14:00:19 +0200

    m

--- modlyGUI/src/animations/hooks/example-cube/Main.js
+++ modlyGUI/src/animations/hooks/example-cube/Main.js
@@ -32,49 +32,47 @@ const CubeExample = () => {
       <div className="adjustmentSection">
            {/*  <input type="checkbox" checked={showGrid} onChange={() => toggleShowGrid(!showGrid)} /> show grid   <br /> */}
            {/*   <input type="checkbox" checked={showCube} onChange={() => toggleShowCube(!showCube)} /> show table  <br /> */}
-            <h3>Surface</h3>
-
-            <span>settableWidth:</span> 
-            <input type="range" id="rs-range-line" min="0" max="1000"  value={cartObject.tableWidth} onChange={e => setCartObject({...cartObject, tableWidth: e.target.value})} />        
-            {cartObject.tableWidth} cm <br />
+            <h3 className="adjustmentTitle">Surface</h3>
+            <p className="testo">Width:</p> 
+            <input type="range" id="rs-range-line" min="0" max="1000" step="10"  value={cartObject.tableWidth} onChange={e => setCartObject({...cartObject, tableWidth: e.target.value})} />        
+            <p className="defege">{cartObject.tableWidth} cm</p> <br />
             
-            <span>settableLength:</span> 
+            <p className="testo">Length:</p> 
             <input type="range" id="rs-range-line" min="0" max="10" value={cartObject.tableLength} onChange={e => setCartObject({...cartObject, tableLength: e.target.value})} />        
-            {cartObject.tableLength} cm <br/>
+            <p className="defege">{cartObject.tableLength} cm</p> <br/>
 
-            <span>tableHeight:</span>
+            <p className="testo">Height:</p>
             <input type="range" id="rs-range-line" min="0" max="10" value={cartObject.tableHeight} onChange={e => setCartObject({...cartObject, tableHeight: e.target.value})} />         
-            {cartObject.tableHeight} cm 
+            <p className="defege">{cartObject.tableHeight} cm</p> 
 
-     <br /> <h3>Legs</h3>
-            <span>setlegWidth:</span> 
+     <br /> <h3 className="adjustmentTitle">Legs</h3>
+            <p className="testo">Width:</p> 
             <input type="range" id="rs-range-line" min="0" max="10" value={cartObject.legWidth} onChange={e => setCartObject({...cartObject, legWidth: e.target.value})} />      
-            {cartObject.legWidth} cm <br />
+            <p className="defege">{cartObject.legWidth} cm</p> <br />
 
-            <span>setlegLength:</span> 
+            <p className="testo">Length:</p> 
             <input type="range" id="rs-range-line" min="0" max="10" value={cartObject.legLength} onChange={e => setCartObject({...cartObject, legLength: e.target.value})} />      
-            {cartObject.legLength} cm <br />
+            <p className="defege">{cartObject.legLength} cm</p> <br />
 
-            <span>setlegHeight:</span> 
+            <p className="testo">Height:</p> 
             <input type="range" id="rs-range-line" min="0" max="10" value={cartObject.legHeight} onChange={e => setCartObject({...cartObject, legHeight: e.target.value})} />       
-            {cartObject.legHeight} cm <br />
+            <p className="defege">{cartObject.legHeight} cm</p> <br />
 
-            <span>setlegAxisX:</span> 
+            <p className="testo">X-Axis:</p> 
             <input type="range" id="rs-range-line" min="0" max="10" value={cartObject.legAxisX} onChange={e => setCartObject({...cartObject, legAxisX: e.target.value})} />  
-            {cartObject.legAxisX} cm <br />
+            <p className="defege">{cartObject.legAxisX} cm</p> <br />
 
-            <span>setlegAxisY:</span> 
+            <p className="testo">Y-Axis:</p> 
             <input type="range" id="rs-range-line" min="0" max="10" value={cartObject.legAxisY} onChange={e => setCartObject({...cartObject, legAxisY: e.target.value})} />  
-            {cartObject.legAxisY} cm <br />
+            <p className="defege">{cartObject.legAxisY} cm</p> <br />
 
-            <h3>Other:</h3>
-            <span>RotationSpeed:</span> <input type="range" id="rs-range-line" className="rs-range" min="0" max="10" onChange={e => setCartObject({...cartObject, rotationSpeed: e.target.value/200})} />  
-            <span> {cartObject.rotationSpeed}</span>
+            <h3 className="adjustmentTitle">Other</h3>
+            <p className="testo">RotationSpeed:</p> <input type="range" id="rs-range-line" className="rs-range" min="0" max="10" onChange={e => setCartObject({...cartObject, rotationSpeed: e.target.value/200})} />  
+            <p className="testo"> {cartObject.rotationSpeed}</p>
 <br/>
 <hr/>
-
-            <span className="finalPriceSpan">Price: {(cartObject.tableWidth + cartObject.tableLength) * 111}€</span>
-            <AddToCartButton meshObjectValues={cartObject}/>
+                <AddToCartButton meshObjectValues={cartObject}/>
+                <p className="finalPrice">{(cartObject.tableWidth + cartObject.tableLength) * 111} €</p>
         </div>
     </SceneManager >
   );
