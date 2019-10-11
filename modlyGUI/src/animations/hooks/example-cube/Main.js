b4cce5f3721854582e083096a4a415eda5123b04
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2019-10-11 09:06:20 +0200

    m

--- modlyGUI/src/animations/hooks/example-cube/Main.js
+++ modlyGUI/src/animations/hooks/example-cube/Main.js
@@ -39,8 +39,7 @@ const CubeExample = () => {
       {showCube && <Table cartObject={cartObject}/>}
 
       <div className="adjustmentSection">
-           {/*  <input type="checkbox" checked={showGrid} onChange={() => toggleShowGrid(!showGrid)} /> show grid   <br /> */}
-           {/*   <input type="checkbox" checked={showCube} onChange={() => toggleShowCube(!showCube)} /> show table  <br /> */}
+              
             <Accordion title="Surface">
 
       <div className="isParent">
@@ -65,29 +64,31 @@ const CubeExample = () => {
      <br />  <Accordion title="Legs">
             <p className="adjustTitle">Width</p> 
             <input type="range" id="rs-range-line" min="0" max="10" value={cartObject.legWidth} onChange={e => setCartObject({...cartObject, legWidth: e.target.value})} />      
-            <p className="adjustmentValue">{cartObject.legWidth} cm</p> <br />
+            <p className="adjustmentValue">{cartObject.legWidth}</p> <br />
 
             <p className="adjustTitle">Length</p> 
             <input type="range" id="rs-range-line" min="0" max="10" value={cartObject.legLength} onChange={e => setCartObject({...cartObject, legLength: e.target.value})} />      
-            <p className="adjustmentValue">{cartObject.legLength} cm</p> <br />
+            <p className="adjustmentValue">{cartObject.legLength}</p> <br />
 
             <p className="adjustTitle">Height</p> 
             <input type="range" id="rs-range-line" min="0" max="10" value={cartObject.legHeight} onChange={e => setCartObject({...cartObject, legHeight: e.target.value})} />       
-            <p className="adjustmentValue">{cartObject.legHeight} cm</p> <br />
+            <p className="adjustmentValue">{cartObject.legHeight}</p> <br />
 
             <p className="adjustTitle"> <i class="fas fa-question-circle"></i> X-Axis</p> 
             <input type="range" id="rs-range-line" min="0" max="10" value={cartObject.legAxisX} onChange={e => setCartObject({...cartObject, legAxisX: e.target.value})} />  
-            <p className="adjustmentValue">{cartObject.legAxisX} cm</p> <br />
+            <p className="adjustmentValue">{cartObject.legAxisX}</p> <br />
 
             <p className="adjustTitle"> <i class="fas fa-question-circle"></i> Y-Axis</p> 
             <input type="range" id="rs-range-line" min="0" max="10" value={cartObject.legAxisY} onChange={e => setCartObject({...cartObject, legAxisY: e.target.value})} />  
-            <p className="adjustmentValue">{cartObject.legAxisY} cm</p> <br />
+            <p className="adjustmentValue">{cartObject.legAxisY}</p> <br />
             </Accordion>
 
             <br />  <Accordion title="Other">
             <p className="adjustTitle">Rotation</p> 
             <input type="range" id="rs-range-line" className="rs-range" min="0" max="10" onChange={e => setCartObject({...cartObject, rotationSpeed: e.target.value/200})} />  
             <p className="adjustmentValue"> {cartObject.rotationSpeed}</p>
+            <input type="checkbox" checked={showGrid} onChange={() => toggleShowGrid(!showGrid)} /> show grid   <br /> 
+            <input type="checkbox" checked={showCube} onChange={() => toggleShowCube(!showCube)} /> show table  <br /> 
             </Accordion>
 <br/>
 <hr/>
