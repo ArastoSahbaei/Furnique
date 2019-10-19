2a409a4734e80413c8ab5487dcb74a146fd462a0
arasto <Arasto.Sahbaei@gmail.com>
2019-10-19 14:57:03 +0200

    m

--- modlyGUI/src/components/footer/Footer.js
+++ modlyGUI/src/components/footer/Footer.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import './Footer.css'
 import SocialMedia from './socialmedia/SocialMedia'
-import SubHeader from '../../components/navigationbar/subheader/SubHeader'
 
 export default function Footer() {
     return (
         <div className="footerWrapper">
-            <SubHeader/>
                <ul className="footerList">
                     <li className="footerList"> COPYRIGHT © 2019, NyttNamn AB</li>
                     <SocialMedia/>
