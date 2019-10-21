4cc07da55dd53aa3e7e192943e60a888b0418516
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2019-10-21 14:05:42 +0200

    updated product entities

--- modlyServer/src/main/java/com/modon/customisation/entity/Product.java
+++ modlyServer/src/main/java/com/modon/customisation/entity/Product.java
@@ -11,29 +11,58 @@ public class Product {
     @Column(name = "PRODUCT_ID", updatable = false, nullable = false)
     private Integer productId;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "USER_ORDER__ID")
     private UserOrder userOrder;
 
-    @Column(name = "PRODUCT_NAME", length = 100, nullable = false)
+    @Column(length = 100, nullable = false)
     private String productName;
 
-    @Column(name = "PRODUCT_DESCRIPTION", length = 255, nullable = false)
-    private String productDescription;
+    @Column(length = 65, nullable = false)
+    private String surfaceMaterial;
 
-    @Column(name = "WIDTH", length = 15, nullable = false)
-    private Integer width;
+    @Column(length = 15, nullable = false)
+    private Integer surfaceWidth;
 
-    @Column(name = "HEIGHT", length = 15, nullable = false)
-    private Integer height;
+    @Column(length = 15, nullable = false)
+    private Integer surfaceHeight;
 
-    @Column(name = "DEPTH", length = 15, nullable = false)
-    private Integer depth;
+    @Column(length = 15, nullable = false)
+    private Integer surfaceLength;
 
-    @Column(name = "COLOR", length = 65, nullable = false)
-    private String color;
+    @Column(length = 65, nullable = false)
+    private String legsMaterial;
+
+    @Column(length = 15, nullable = false)
+    private Integer legsWidth;
+
+    @Column(length = 15, nullable = false)
+    private Integer legsHeight;
+
+    @Column(length = 15, nullable = false)
+    private Integer legsLength;
 
     public Product() {}
+
+    public String getLegsMaterial() { return legsMaterial; }
+
+    public void setLegsMaterial(String legsMaterial) { this.legsMaterial = legsMaterial; }
+
+    public Integer getLegsWidth() { return legsWidth; }
+
+    public void setLegsWidth(Integer legsWidth) { this.legsWidth = legsWidth; }
+
+    public Integer getLegsHeight() { return legsHeight; }
+
+    public void setLegsHeight(Integer legsHeight) { this.legsHeight = legsHeight; }
+
+    public Integer getLegsLength() { return legsLength; }
+
+    public void setLegsLength(Integer legsLength) { this.legsLength = legsLength; }
+
+    public String getSurfaceMaterial() { return surfaceMaterial; }
+
+    public void setSurfaceMaterial(String surfaceMaterial) { this.surfaceMaterial = surfaceMaterial; }
 
     public Integer getProductId() {
         return productId;
@@ -59,43 +88,27 @@ public class Product {
         this.productName = productName;
     }
 
-    public String getProductDescription() {
-        return productDescription;
+    public Integer getSurfaceWidth() {
+        return surfaceWidth;
     }
 
-    public void setProductDescription(String productDescription) {
-        this.productDescription = productDescription;
+    public void setSurfaceWidth(Integer surfaceWidth) {
+        this.surfaceWidth = surfaceWidth;
     }
 
-    public Integer getWidth() {
-        return width;
+    public Integer getSurfaceHeight() {
+        return surfaceHeight;
     }
 
-    public void setWidth(Integer width) {
-        this.width = width;
+    public void setSurfaceHeight(Integer surfaceHeight) {
+        this.surfaceHeight = surfaceHeight;
     }
 
-    public Integer getHeight() {
-        return height;
+    public Integer getSurfaceLength() {
+        return surfaceLength;
     }
 
-    public void setHeight(Integer height) {
-        this.height = height;
-    }
-
-    public Integer getDepth() {
-        return depth;
-    }
-
-    public void setDepth(Integer depth) {
-        this.depth = depth;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
+    public void setSurfaceLength(Integer surfaceLength) {
+        this.surfaceLength = surfaceLength;
     }
 }
