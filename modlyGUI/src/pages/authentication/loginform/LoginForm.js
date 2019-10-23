a88a4fc85a1521fb0a1fe2cd367b5fbf950409e9
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2019-10-23 11:10:20 +0200

    updated css login

--- modlyGUI/src/pages/authentication/loginform/LoginForm.js
+++ modlyGUI/src/pages/authentication/loginform/LoginForm.js
@@ -6,13 +6,16 @@ const Login = () => {
         <div className="loginCard">
             <span className="wellcomeBackTitle">Hey! we're glad to see you again.</span>
             <form action="action_page.php" method="post">
-
-                <input className="emailInput" name="email" type="text" placeholder="Email" required/> <br/>
-                <input className="passwordInput" type="password" placeholder="Password" required></input> <br/>
-
+             <div className="inputFields">
+                <label><i className="far fa-envelope fa-2x"/></label>
+                 <input className="emailInput" name="email" type="text" placeholder="Email" required/> <br/>
+                <label><i className="fas fa-lock fa-2x"/></label>
+                 <input className="passwordInput" type="password" placeholder="Password" required></input> <br/>
+            </div>
                 <button className="loginButton" name="psw" type="submit">Login</button> <br />
-                <span class="psw"><a href="#">Forgot Password?</a></span>
+                <p className="forgotPassword"><a href="#">Forgot Password?</a></p>
             </form>
+
         </div>
     )
 }
