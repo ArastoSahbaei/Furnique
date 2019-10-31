05a030dfff498b89d0e7d4719e8515d0b80606ae
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2019-10-31 15:52:33 +0100

    backup

--- modlyGUI/src/pages/authentication/emailverification/EmailVerification.js
+++ modlyGUI/src/pages/authentication/emailverification/EmailVerification.js
@@ -1,11 +1,13 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import {verifyEmail} from '../../../shared/api/UserAPI'
 import './EmailVerification.css'
 
 const EmailVerification = () => {
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         verifyEmail(window.location.href.split('?').reverse()[0])
+        setLoading(false)
         return () => {
             /* cleanup */
         };
@@ -18,8 +20,9 @@ const EmailVerification = () => {
             1. Get user By verificationToken
             2. check if user boolean is true
             3. display 'account verified'
-
         {/* (userByToken.isEnabled) ? 'Success' : 'Failed' */}
+        
+        {loading ? <h1>Account verification ongoing</h1> : <h1>Account Verified</h1>}
 
         </div>
     )
