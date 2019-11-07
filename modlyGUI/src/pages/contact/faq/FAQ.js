928cfa229ed84f203d10162e9fe936233681d137
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2019-11-07 08:59:26 +0100

    commit

--- modlyGUI/src/pages/contact/faq/FAQ.js
+++ modlyGUI/src/pages/contact/faq/FAQ.js
@@ -4,26 +4,30 @@ import Accordion from '../../../components/accordion/Accordion'
 
 export default function FAQ() {
     return (
-        <div>
-           <Accordion title="Surface">
+        <div className="faqContainer">
+        <h1>Most Common Frequently Asked Questions</h1>
+           <Accordion title="Where is Furnique furniture made?">
                 <span>Answer</span>
            </Accordion>
-           <Accordion title="Surface">
+           <Accordion title="Will you release any new products?">
                 <span>Answer</span>
            </Accordion>
-           <Accordion title="Surface">
+           <Accordion title="Are you hiring?">
                 <span>Answer</span>
            </Accordion>
-           <Accordion title="Surface">
+           <Accordion title="Do you have any physical stores?">
                 <span>Answer</span>
            </Accordion>
-           <Accordion title="Surface">
+           <Accordion title="How do I assemble Tylko furniture?">
                 <span>Answer</span>
            </Accordion>
-           <Accordion title="Surface">
+           <Accordion title="Can the furniture be dismantled after it has been assembled?">
                 <span>Answer</span>
            </Accordion>
-           <Accordion title="Surface">
+           <Accordion title="Which parts of my furniture can I customise?">
+                <span>Answer</span>
+           </Accordion>
+           <Accordion title="Which parts of my furniture can I customise?">
                 <span>Answer</span>
            </Accordion>
         </div>
