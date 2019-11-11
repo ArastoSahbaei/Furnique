c68fb52c18625e016a7ed7dd1fd685ab6c3100e7
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2019-11-11 13:19:58 +0100

    updated design

--- modlyGUI/src/pages/home/climatesection/ClimateSection.js
+++ modlyGUI/src/pages/home/climatesection/ClimateSection.js
@@ -6,8 +6,11 @@ const ClimateSection = () => {
     return (
         <div className="climateSectionWrapper">
             <img className="treeLogo" src={TreeLogo} alt=""/>
-            <p className="climateTitle">Furnique 3 Miljön <br/> Vi klimatkompenserar för varje bord vi tillverkar.</p>
+            <div className="climateTitleContainer">
+            <span className="climateTitle">Furnique <i className="fas fa-heartbeat"></i> Miljön </span> 
+            <p className="climateParagraph">Vi klimatkompenserar för varje bord vi tillverkar.</p>
             <button className="climateReadMore">LÄS MER OM HUR</button>
+            </div>
         </div>
     )
 }
