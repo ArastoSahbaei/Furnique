3bf6f81b405f9756568e10a5bb1d864a2719ea3b
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2019-11-13 13:16:52 +0100

    updated a lil

--- modlyGUI/src/error/emailVerification/EmailVerificationFailed.js
+++ modlyGUI/src/error/emailVerification/EmailVerificationFailed.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import './EmailVerificationFailed.css'
 
 const EmailVerificationFailed = () => {
     return (
-        <div>
-            <h1>Theres been an issue</h1>
+        <div className="emailVerificationFailedWrapper">
+            <span className="verificationFailedTitle">Unfortunatley, <br/> there has been an issue.</span> <br/>
+            <span className="verificationFailedSubTitle">You did not verify your user in time.</span> <br />
+            <button className="sendNewVerificationMailButton">Send me a new verification email</button>
         </div>
     )
 }
