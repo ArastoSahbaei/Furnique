3849caa03b6a2de56b346f310d2caf4488013e92
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2019-11-13 13:17:45 +0100

    m

--- modlyGUI/src/components/languageHandler/LanguageHandler.js
+++ modlyGUI/src/components/languageHandler/LanguageHandler.js
@@ -15,7 +15,7 @@ const LanguageHandler = () => {
 
             <div className="dropdown-content">
              <a href="#"> <img className="flag" src={Sweden} alt="SW"/>   SE </a>
-             <a href="#"> <img className="flag" src={UK} alt="UK"/>       UK </a>
+             <a href="#"> <img className="flag" src={UK} alt="UK"/>       EN </a>
              <a href="#"> <img className="flag" src={Denmark} alt="DK"/>  DK </a>
              <a href="#"> <img className="flag" src={Norway} alt="NO"/>   NO </a>
             </div>
