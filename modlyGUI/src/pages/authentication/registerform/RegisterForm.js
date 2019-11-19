7eea48a2322607a467862688c7e97ef0956b9249
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2019-11-19 10:57:46 +0100

    m

--- modlyGUI/src/pages/authentication/registerform/RegisterForm.js
+++ modlyGUI/src/pages/authentication/registerform/RegisterForm.js
@@ -15,13 +15,13 @@ const RegisterForm = () => {
      }
 
     return (
-        <div className="registerAccountContainer">
-             <p className="registerAccountTitle"><i className="fas fa-reply-all"/>New to <span className="registerAccountTitle2">Furnique</span> ?</p>
-             <input className="input-field1" type="text"     placeholder="Firstname (Optional)"     onChange={handleChange('firstName')}/>
-             <input className="input-field3" type="text"     placeholder="Email" name="email"       onChange={handleChange('email')}/>
-             <input className="input-field4" type="password" placeholder="Password" name="psw"      onChange={handleChange('password')}/>   
-             <button onClick={() => registerNewUser(user)} type="submit" className="registerAccountButton">Register Account</button>
-        </div>
+    <form className="registerAccountContainer">
+        <p className="registerAccountTitle"><i className="fas fa-reply-all"/>New to <span className="registerAccountTitle2">Furnique</span> ?</p>
+            <input className="input-field1" type="text"      placeholder="Firstname (Optional)" required       onChange={handleChange('firstName')}/>
+            <input className="input-field3" type="email"     placeholder="Email"                required       onChange={handleChange('email')}/>
+            <input className="input-field4" type="password"  placeholder="Password"             required       onChange={handleChange('password')}/>   
+        <button onClick={() => registerNewUser(user)} type="submit" className="registerAccountButton">Register Account</button>
+    </form>
     )
 }
 
