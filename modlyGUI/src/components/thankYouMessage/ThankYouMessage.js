7cc2c7a41b7ed34b02bb2b0ad13d09de4dc16d33
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2019-11-19 16:14:21 +0100

    m

---
+++ modlyGUI/src/components/thankYouMessage/ThankYouMessage.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './ThankYouMessage.css'
+
+export const ThankYouMessage = () => {
+    return (
+        <div>
+            <p className="thankYouMessage">Welcome to Furnique <br/> 
+            <span className="confirmYourMail">We've sent you a mail, please confirm your account.</span>
+            </p>
+
+
+            <div className="thankYouMessageButtonContainer">
+                <li className="thankYouMessageButton1"> <button className="browseProducts"> <span>Browse products </span> </button> <br/> </li>
+                <li className="thankYouMessageButton2"> <button className="createYourOwn">  <span>Design your own </span> </button> <br/> </li>
+                <li className="thankYouMessageButton3"> <button className="getInspired">    <span>Get inspiration </span> </button> <br/> </li>
+            </div>
+        </div>
+    )
+}
--- modlyGUI/src/pages/authentication/registerform/RegisterForm.js
+++ modlyGUI/src/pages/authentication/registerform/RegisterForm.js
@@ -1,32 +1,45 @@
 import React, {useState} from 'react'
-import {registerNewUser} from '../../../shared/api/UserAPI'
+import Axios from 'axios'
+import {ThankYouMessage} from '../../../components/thankYouMessage/ThankYouMessage'
 import './RegisterForm.css'
 
 const RegisterForm = () => {
-
+    const [loading, setLoading] = useState(false)
+    const [registered, setRegistered] = useState(false)
     const [user, setUser] = useState({
         firstName: "",
         email: "",
         password: "",
     });
 
+    const registerNewUser = (user) => { Axios.post('http://localhost:8080/register', user)
+      .then(function (response) {
+        return setRegistered(true);
+       })
+       .catch(function (error) {
+         return alert(error) + setLoading(false);
+       });
+     }
+
     const handleChange = key => event => {
         setUser({...user, [key]: event.target.value});
      }
 
      const handleSubmit = event => {
+        setLoading(true);
         registerNewUser(user);
         event.preventDefault();
      }
 
     return (
-    <form onSubmit={handleSubmit} className="registerAccountContainer">
-        <p className="registerAccountTitle"> <i className="fas fa-reply-all"/> New to <span className="registerAccountTitle2">Furnique</span> ?</p>
-            <input className="input-field1" type="text"      placeholder="Firstname"            required       onChange={handleChange('firstName')}/>
-            <input className="input-field3" type="email"     placeholder="Email"                required       onChange={handleChange('email')}/>
-            <input className="input-field4" type="password"  placeholder="Password" pattern=".{8,}" required title="8 characters minimum" onChange={handleChange('password')}/>   
-        <button type="submit" className="registerAccountButton"> Register Account </button>
-    </form>
+        registered ? <ThankYouMessage /> :
+        <form onSubmit={handleSubmit} className="registerAccountContainer">
+            <p className="registerAccountTitle"> <i className="fas fa-reply-all"/> New to <span className="registerAccountTitle2">Furnique</span> ?</p>
+            <i className="fas fa-signature" />  <input className="input-field1" type="text"      placeholder="Firstname"            required       onChange={handleChange('firstName')}/>
+            <i className="far fa-envelope" />   <input className="input-field3" type="email"     placeholder="Email"                required       onChange={handleChange('email')}/>
+            <i className="fas fa-lock" />       <input className="input-field4" type="password"  placeholder="Password" pattern=".{8,}" required title="8 characters minimum" onChange={handleChange('password')}/>   
+            <button type="submit" className="registerAccountButton"> {loading ? <span>LOADING</span> : <span>Register Account</span> } </button>
+        </form>
     )
 }
 
