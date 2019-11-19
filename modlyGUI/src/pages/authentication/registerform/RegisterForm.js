b50404ffd7c66935d6f320cef5e1d9e03d57c561
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2019-11-19 13:16:36 +0100

    m

--- modlyGUI/src/pages/authentication/registerform/RegisterForm.js
+++ modlyGUI/src/pages/authentication/registerform/RegisterForm.js
@@ -21,11 +21,11 @@ const RegisterForm = () => {
 
     return (
     <form onSubmit={handleSubmit} className="registerAccountContainer">
-        <p className="registerAccountTitle"><i className="fas fa-reply-all"/>New to <span className="registerAccountTitle2">Furnique</span> ?</p>
-            <input className="input-field1" type="text"      placeholder="Firstname (Optional)" required       onChange={handleChange('firstName')}/>
+        <p className="registerAccountTitle"> <i className="fas fa-reply-all"/> New to <span className="registerAccountTitle2">Furnique</span> ?</p>
+            <input className="input-field1" type="text"      placeholder="Firstname"            required       onChange={handleChange('firstName')}/>
             <input className="input-field3" type="email"     placeholder="Email"                required       onChange={handleChange('email')}/>
-            <input className="input-field4" type="password"  placeholder="Password"             required       onChange={handleChange('password')}/>   
-        <button /* onClick={() => handleSubmit()} */ type="submit" className="registerAccountButton"> Register Account </button>
+            <input className="input-field4" type="password"  placeholder="Password" pattern=".{8,}" required title="8 characters minimum" onChange={handleChange('password')}/>   
+        <button type="submit" className="registerAccountButton"> Register Account </button>
     </form>
     )
 }
