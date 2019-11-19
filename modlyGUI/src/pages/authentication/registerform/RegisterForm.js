37bc34be0124d79839d7978ea8fe06aba7862f02
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2019-11-19 11:07:23 +0100

    not reloading

--- modlyGUI/src/pages/authentication/registerform/RegisterForm.js
+++ modlyGUI/src/pages/authentication/registerform/RegisterForm.js
@@ -14,13 +14,18 @@ const RegisterForm = () => {
         setUser({...user, [key]: event.target.value});
      }
 
+     const handleSubmit = event => {
+        registerNewUser(user);
+        event.preventDefault();
+     }
+
     return (
-    <form className="registerAccountContainer">
+    <form onSubmit={handleSubmit} className="registerAccountContainer">
         <p className="registerAccountTitle"><i className="fas fa-reply-all"/>New to <span className="registerAccountTitle2">Furnique</span> ?</p>
             <input className="input-field1" type="text"      placeholder="Firstname (Optional)" required       onChange={handleChange('firstName')}/>
             <input className="input-field3" type="email"     placeholder="Email"                required       onChange={handleChange('email')}/>
             <input className="input-field4" type="password"  placeholder="Password"             required       onChange={handleChange('password')}/>   
-        <button onClick={() => registerNewUser(user)} type="submit" className="registerAccountButton">Register Account</button>
+        <button type="submit" className="registerAccountButton">Register Account</button>
     </form>
     )
 }
