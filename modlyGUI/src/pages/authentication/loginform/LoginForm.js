6df9bc26c61da805a672c9651865ce6eb4010fb9
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2019-11-20 15:41:39 +0100

    updated loginform

--- modlyGUI/src/pages/authentication/loginform/LoginForm.js
+++ modlyGUI/src/pages/authentication/loginform/LoginForm.js
@@ -4,15 +4,20 @@ import './LoginForm.css'
 const Login = () => {
     return (
         <div className="loginCard">
+            <div className="titleDiv">
             <span className="wellcomeBackTitle">Hey, we're glad to see you again!</span>
+            </div>
+            <hr/>
             <form method="post">
              <div className="inputFields">
-                <label><i className="far fa-envelope fa-2x"/></label>
-                 <input className="emailInput" name="email" type="text" placeholder="Email" required/> <br/>
-                <label><i className="fas fa-lock fa-2x"/></label>
-                 <input className="passwordInput" type="password" placeholder="Password" required></input> <br/>
+                <label> <span className="loginFormEmail"><i className="fas fa-key"/> Email</span> </label>
+                 <input className="emailInput" name="email" type="email" placeholder=" Your Email" required/> 
+                 <br/>
+                <label> <span className="loginFormPassword"><i className="fas fa-key"/> Password</span> </label>
+                 <input className="passwordInput" type="password" placeholder=" Your Password" required></input> 
+                 <br/>
             </div>
-                <button className="loginButton" name="psw" type="submit">Login</button> <br />
+                <button className="loginButton" name="psw" type="submit"><span>Login</span></button> <br />
                 <p className="forgotPassword"><a href="#">Forgot Password?</a></p>
             </form>
 
