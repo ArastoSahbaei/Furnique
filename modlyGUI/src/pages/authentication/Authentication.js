2a3a55826defab310dfa5919c96c63a3256fe8f0
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2019-11-20 16:48:23 +0100

    seperated floatingSquares

--- modlyGUI/src/pages/authentication/Authentication.js
+++ modlyGUI/src/pages/authentication/Authentication.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Authentication.css'
+import {FloatingSquares} from '../../shared/design/floatingSquares/FloatingSquares'
 import LoginForm from './loginform/LoginForm'
 import RegisterForm from './registerform/RegisterForm'
 
 const Authentication = () => {
     return (
         <div className="authenticationLoginContainer">
+          <FloatingSquares />
           <LoginForm />
           <RegisterForm />
         </div>
---
+++ modlyGUI/src/shared/design/floatingSquares/FloatingSquares.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './FloatingSquares.css'
+
+export const FloatingSquares = () => {
+    return (
+                    <div className="area" >
+          <ul className="circles">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+    )
+}
