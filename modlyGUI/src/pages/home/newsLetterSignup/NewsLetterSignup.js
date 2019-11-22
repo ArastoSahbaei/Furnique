b40c61ebda850771ec01064740bb5e0af54a31fa
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2019-11-22 15:57:18 +0100

    subscribe

--- modlyGUI/src/pages/home/newsLetterSignup/NewsLetterSignup.js
+++ modlyGUI/src/pages/home/newsLetterSignup/NewsLetterSignup.js
@@ -13,7 +13,7 @@ export const NewsLetterSignup = () => {
             <form className="newsLetterSignupForm">
             <span className="newsLetterHeadTitle">Inspiration & special offers? Sign up to our newsletter</span> 
                     <input className="newsLetterEmailInput" name="email" type="email" placeholder=" Your Email" required/> 
-                    <button onClick={() => signUpToNewsLetter()} className="signMeUpButton" type="submit"> { loading ? "LOADING..." : "SIGN ME UP !" } </button>
+                    <button onClick={() => signUpToNewsLetter()} className="signMeUpButton" type="submit"> { loading ? "LOADING..." : "SUBSCRIBE" } </button>
             </form>
             <span className="unsubscribeMessage">No spam. Unsubscribe at anytime.</span>
         </div>
