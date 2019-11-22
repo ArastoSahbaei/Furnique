474cf34ef5cb7cd99cf84dfe1bfd8cedf58b72b8
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2019-11-22 15:41:49 +0100

    m

--- modlyGUI/src/pages/home/newsLetterSignup/NewsLetterSignup.js
+++ modlyGUI/src/pages/home/newsLetterSignup/NewsLetterSignup.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
 import './NewsLetterSignup.css'
 
 export const NewsLetterSignup = () => {
@@ -7,6 +7,7 @@ export const NewsLetterSignup = () => {
     const signUpToNewsLetter = () => { setLoading(true) }
 
     return (
+        loading ? <h1>Thank you for signing up</h1> :
         <div className="newsLetterSignupContainer">
             <hr className="newsLetterHR"/>
             <form className="newsLetterSignupForm">
